3d1e7c6d38625adf347da113db56156e61b69cbe
ashoomky <ashlee@shum.co.nz>
2026-02-02 16:26:47 +1300

    about me minor tweaks

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,16 +9,16 @@ const AboutMe = () => {
     const [isDragging, setIsDragging] = useState(false);
     const [dragStart, setDragStart] = useState(0);
 
-    const handleMouseDown = (e) => {
+    const handleMouseDown = (event) => {
         setIsDragging(true);
-        setDragStart(e.clientX);
+        setDragStart(event.clientX);
     }
 
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (event) => {
         // if user not dragging, do nothing
         if (!isDragging) return;
         // calculate distance dragged
-        const dragDistance = e.clientX - dragStart;
+        const dragDistance = event.clientX - dragStart;
         if (dragDistance > 50) {
             //  if on last photo, loop back to first
             setCurrentIndex(prevIndex => prevIndex == 0 ? photos.length - 1 : prevIndex - 1);
@@ -53,11 +53,18 @@ const AboutMe = () => {
                     
                     {photos.map((photo, index) => {
                         const rotation = (index - currentIndex) * 5 - 1 + Math.random() * 3;
-                        const zIndex = photos.length - Math.abs(index - currentIndex);
-                        const opacity = index === currentIndex ? 1 : 0.5;
-                        const scale = index === currentIndex ? 1 : 0.8;
+                        const zIndex = photos.length - Math.abs(index - currentIndex); // setting visibility priority for each photo
+                        const opacity = index === currentIndex ? 1 : 0.5; // current photo fully visible
+                        const scale = index === currentIndex ? 1 : 0.8; // current photo full size
+                        const photoAnimation = {
+                                    transform: `rotate(${rotation}deg) scale(${scale})`,
+                                    zIndex: zIndex,
+                                    opacity: opacity,
+                                }
                         // only render current, previous, and next photo
                         if (Math.abs(index - currentIndex) > 1 &&
+                                // checking edge cases: last photo can appear when current is first photo
+                                // and first photo appears when current is last photo
                             !(currentIndex === 0 && index === photos.length - 1) &&
                             !(currentIndex === photos.length - 1 && index === 0)) {
                             return null;
@@ -68,27 +75,20 @@ const AboutMe = () => {
                                 src={photo}
                                 alt={`Photo ${index + 1}`}
                                 className="absolute w-[300px] h-[300px] object-cover rounded-lg shadow-lg transition-all duration-300 hover:scale-103"
-                                style={{
-                                    transform: `rotate(${rotation}deg) scale(${scale})`,
-                                    zIndex: zIndex,
-                                    opacity: opacity,
-                                }}
+                                style={photoAnimation}
                                 draggable={false}
                             />   
-                    )})
+                        )})
                     }
                 </div>
     
                 {/* description */}
                 <div className="flex-1 md:w-1/2 m-10 text-left">
                     <p className="pb-4">
-                        Hiya! I am currently a student at the University of Auckland in my penultimate year, studying a Bachelor of Science majoring in Computer Science. I have a passion for creating beautiful and functional web applications that solve real world problems while also making a positive impact on people. 
+                        Hiya! I am currently a student at the University of Auckland in my penultimate year, studying a Bachelor of Science majoring in Computer Science. I have a passion for creating aesthetic and functional web applications that solve real world problems, while also making a positive impact on people. 
                     </p>
                     <p className="pb-4">
                         In my free time I enjoy editing videos, travelling, gymming, listening to music and spending time with loved ones. I'm always eager to learn and take on new challenges, and make genuine connections with people.
-                    </p>
-                    <p className="pb-4">
-                        Thank you for visiting my website :)
                     </p>
                     <p className="italic text-gray-600 text-sm">
                         [swipe photos left and right]
